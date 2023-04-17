4eb201d334925d149379f3d0f67b7b671ef5a3f2
Rohan Dharmadhikari <drohan.1994@gmail.com>
2023-04-17 15:56:30 +0530

    new results and assets

--- create_gif_array.py
+++ create_gif_array.py
@@ -1,23 +1,48 @@
 import imageio
-import numpy as np    
+import numpy as np
 
-#Create reader object for the gif
-gif1 = imageio.get_reader('file1.gif')
-gif2 = imageio.get_reader('file2.gif')
+from PIL import Image
 
-#If they don't have the same number of frame take the shorter
-number_of_frames = min(gif1.get_length(), gif2.get_length()) 
+gif1 = imageio.get_reader('results/result_vox_chan_priyavrat.gif')
+gif2 = imageio.get_reader('results/result_vox_chan_pyne.gif')
+gif3 = imageio.get_reader('results/result_vox_chan_varun.gif')
+gif4 = imageio.get_reader('results/result_vox_chan_fed.gif')
+gif5 = imageio.get_reader('results/result_vox_chan_nadal.gif')
 
-#Create writer object
-new_gif = imageio.get_writer('output.gif')
+source_img1 = imageio.imread('assets/source_vox_priyavrat.png')
+source_img2 = imageio.imread('assets/source_vox_pyne.jpg')
+source_img3 = imageio.imread('assets/source_vox_varun.jpg')
+source_img4 = imageio.imread('assets/source_vox_fed.jpg')
+source_img5 = imageio.imread('assets/source_vox_nadal.jpg')
 
+source_img1 = np.array(Image.fromarray(source_img1).resize((256, 256))).astype(np.uint8)[:,:,:3]
+source_img2 = np.array(Image.fromarray(source_img2).resize((256, 256))).astype(np.uint8)[:,:,:3]
+source_img3 = np.array(Image.fromarray(source_img3).resize((256, 256))).astype(np.uint8)[:,:,:3]
+source_img4 = np.array(Image.fromarray(source_img4).resize((256, 256))).astype(np.uint8)[:,:,:3]
+source_img5 = np.array(Image.fromarray(source_img5).resize((256, 256))).astype(np.uint8)[:,:,:3]
+
+driving_gif = imageio.get_reader('assets/driving_vox_chan.mp4')
+
+number_of_frames = gif1.get_length()
+
+gif_array = []
 for frame_number in range(number_of_frames):
-    img1 = gif1.get_next_data()
-    img2 = gif2.get_next_data()
-    #here is the magic
-    new_image = np.hstack((img1, img2))
-    new_gif.append_data(new_image)
+    d_img = driving_gif.get_next_data()
+    d_img = np.array(Image.fromarray(d_img).resize((256, 256))).astype(np.uint8)[:,:,:3]
 
-gif1.close()
-gif2.close()    
-new_gif.close()
+    img1 = gif1.get_next_data().astype(np.uint8)[:,:,:3]
+    img2 = gif2.get_next_data().astype(np.uint8)[:,:,:3]
+    img3 = gif3.get_next_data().astype(np.uint8)[:,:,:3]
+    img4 = gif4.get_next_data().astype(np.uint8)[:,:,:3]
+    img5 = gif5.get_next_data().astype(np.uint8)[:,:,:3]
+
+    blank_image = np.ones(shape=source_img1.shape).astype(np.uint8)*255
+    
+    # print(blank_image.shape, source_img1.shape, d_img.shape, img1.shape)
+    new_image1 = np.hstack((blank_image, source_img1, source_img2, source_img3, source_img4, source_img5))
+    new_image2 = np.hstack((d_img, img1, img2, img3, img4, img5))
+
+    new_image = np.vstack((new_image1, new_image2))
+    gif_array.append(new_image)
+
+imageio.mimsave(uri='results/vox_chan.gif', ims=gif_array)
--- run_demo.sh
+++ run_demo.sh
@@ -1,9 +1,9 @@
 python -u demo.py \
         --config config/vox-256.yaml \
         --checkpoint checkpoints/vox.pth.tar \
-		--source_image assets/source_vox_rohan.jpg \
+		--source_image assets/source_vox_shreyansh.jpg \
 		--driving_video assets/driving_vox_chan.mp4 \
-        --result_video results/result_vox_chan_rohan.gif \
+        --result_video results/result_nemo_chan_shreyansh.gif \
         --img_shape '256,256'
 
 exit 0
