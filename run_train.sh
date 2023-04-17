777025748dedfc5c93454df96fea950744eb7d55
Rohan Dharmadhikari <drohan.1994@gmail.com>
2023-04-17 16:07:20 +0530

    checkpoint given

--- run_train.sh
+++ run_train.sh
@@ -1,3 +1,4 @@
-CUDA_VISIBLE_DEVICES=0,1 python run.py --config config/nemo-256.yaml --device_ids 0,1
+CUDA_VISIBLE_DEVICES=0,1 python run.py --config config/nemo-256.yaml --log_dir "log/nemo-256 16_04_23_16.32.22" \
+                    --checkpoint "log/nemo-256 16_04_23_16.32.22/00000009-checkpoint.pth.tar" --device_ids 0,1
 
 exit 0
