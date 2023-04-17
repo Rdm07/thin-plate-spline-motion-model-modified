61742be202eba3b3dd0d2068712c6b5aa9f58ef5
Rohan Dharmadhikari <drohan.1994@gmail.com>
2023-04-17 05:41:37 +0530

    restructuring

--- run_demo.sh
+++ run_demo.sh
@@ -1,9 +1,9 @@
 python -u demo.py \
-        --config config/ted-384.yaml \
-        --checkpoint checkpoints/ted.pth.tar \
-		--source_image assets/source_ted_rohan.jpeg\
-		--driving_video assets/driving_ted_biswas.mp4 \
-        --result_video results/result_ted_biswas_rohan.mp4 \
+        --config config/vox-256.yaml \
+        --checkpoint checkpoints/vox.pth.tar \
+		--source_image assets/source_vox_rohan.jpg \
+		--driving_video assets/driving_vox_chan.mp4 \
+        --result_video results/result_vox_chan_rohan.gif \
         --img_shape '256,256'
 
 exit 0
