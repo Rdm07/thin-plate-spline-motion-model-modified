912f6331bc87b337b76b521d7fb61fae4000784b
Rohan Dharmadhikari <drohan.1994@gmail.com>
2023-04-17 18:19:55 +0530

    something

--- run_demo.sh
+++ run_demo.sh
@@ -1,9 +1,9 @@
 python -u demo.py \
-        --config config/vox-256.yaml \
-        --checkpoint checkpoints/vox.pth.tar \
-		--source_image assets/source_vox_shreyansh.jpg \
+        --config config/nemo-256.yaml \
+        --checkpoint "log/nemo-256 16_04_23_16.32.22/00000009-checkpoint.pth.tar" \
+		--source_image assets/source_vox_priyavrat.png \
 		--driving_video assets/driving_vox_chan.mp4 \
-        --result_video results/result_nemo_chan_shreyansh.gif \
+        --result_video results/result_nemo_chan_priyavrat.gif \
         --img_shape '256,256'
 
 exit 0
